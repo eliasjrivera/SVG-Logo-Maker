e0f612474f6f77b4e0a279a79ccb6fa6436f6fc4
Elias Rivera <eliasjohnrivera@gmail.com>
2023-06-12 16:29:48 -0700

    added js files

---
+++ index.js
@@ -0,0 +1,86 @@
+// packages needed for this appl;ication
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Square, Circle } = require('./lib/shapes');
+
+// svg file creation function
+function writeToFile(answers) {
+    const DEFAULT_FILE_NAME = 'logo.svg';
+
+    let svgCanvas ='<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><g>';
+    svgCanvas += `${answers.shape}`;
+
+    let shapeChoice;
+    if (answers.shape === 'Triangle') {
+        shapeChoice = new Triangle();
+    } else if (answers.shape === 'Square') {
+        shapeChoice = new Square();
+    } else {
+        shapeChoice = new Circle();
+    };
+
+    shapeChoice.setColor(answers.shapeColor);
+    svgCanvas += shapeChoice.generate();
+
+    svgCanvas += `<text x='150' y='130' text-anchor='middle' font-size='40' fill='${answers.textColor}'>${answers.text}</text>`;
+    svgCanvas += "</g></svg>";
+
+    fs.writeFile(DEFAULT_FILE_NAME, svgCanvas, (err) => {
+        err ? console.log(err) : console.log("SVG Logo Generated!");
+    });
+};
+
+// inquirer prompt module
+function inquirerPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message:
+                'What text would you like your logo to have? Up to three characters.',
+                name: 'text',
+                // adding validation function to test character legnth, helped by tutor
+                validate: function (input) {
+                    // creating async hack for inquirer
+                    const done = this.async();
+                    // timeoue to test character length
+                    setTimeout(function() {
+                        if (input.length > 3) {
+                            done('Answer needs to be three characters or less')
+                        }
+                        // shows as null or true
+                        done(null, true);
+                    }, 1000);
+                }
+            },
+            {
+                type: 'input',
+                message:
+                'What text color would you like your logo to have? Use color name or a hexadecimal number.',
+                name: 'textColor',
+            },
+            {
+                type: 'list',
+                message: 'What shape would you like your logo to have?',
+                name: "shape",
+                choices: [
+                    'Triangle', 
+                    'Square', 
+                    'Circle'
+                ]
+            },
+            {
+                type: 'input',
+                message:
+                'What color would you like your shape to have? Use color name or a hexadecimal number.',
+                name: 'shapeColor',
+            },
+])
+    .then(answers => {
+            writeToFile(answers);
+        }
+    )};
+
+
+// calling inquirer prompt
+inquirerPrompt();
